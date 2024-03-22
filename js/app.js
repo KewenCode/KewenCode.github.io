dfd80cf62f11c2c92fd8c7fe66cfece24e09586c
KewenCode <kkllgxn2000.com@163.com>
2024-03-22 21:23:43 +0800

    add renderer select

--- js/app.js
+++ js/app.js
@@ -1,56 +1,5 @@
-// let generate = require.config({
-//   baseUrl: 'js/',
-//   paths: {
-//     "jquery": ["libs/jquery-3.7.1.min"],
-//     "bootstrap": ["libs/bootstrap.bundle"],
-//     "pixi": "libs/pixi.min",
-//     "SorTable": "libs/Sortable.min",  // include require
-//     "selectTag": "selectTag",
-//     "listStation": "listStation",
-//     "nanjing": "nanjing/nanjingList",
-//   },
-//   shim: {
-//     pixi: {
-//       exports: 'PIXI'
-//     },
-//     bootstrap: {
-//       deps: ['jquery']
-//     },
-//     // "SorTable": {
-//     //   exports: "Sortable"
-//     // },
-//     // "selectTag": {
-//     //   deps: ["Sortable"],
-//     // },
-//     //   "drawSprite": {
-//     //     deps: ["PIXI"],
-//     //     exports: "drawSprite"
-//     //   }
-//   }
-// })
-
-// let collectBtn = {}
-
-// generate(["nanjing", "selectTag", "listStation", "jquery", "SorTable"], function (nanjing, selectTag, listStation) {
-
-//   collectBtn.removeTag = function (_this, tag) { selectTag.removeTag(_this, tag) }
-//   collectBtn.removeItemStation = function (start, end) { listStation.removeItemStation(start, end) }
-//   collectBtn.stationClick = function (_this) { listStation.stationClick(_this) }
-//   collectBtn.updateBtn = function () { console.log(111); }
-//   // 创建画布
-//   const routeMap = new PIXI.Application({
-//     backgroundColor: '#FFF',
-//     hello: true,
-//     // antialias: true,  //抗锯齿
-//     // resolution: 1, //分辨率
-//     sortableChildren: true,
-//   });
-//   // PIXI加入指定DOM
-//   const wrap = document.querySelector("#map_wrap");
-//   wrap.appendChild(routeMap.canvas);
-
-// })
-
+const baseUrl = new URL(document.documentURI)
+const paramUrl = new URLSearchParams(baseUrl.search)
 
 // 创建画布
 const routeMap = new PIXI.Application();
@@ -61,10 +10,24 @@ await routeMap.init({
   resolution: 1, //分辨率
   sortableChildren: true,
   autoStart: false, // 取消自动渲染
+  preference: paramUrl.get('render'), //手动选择render
 });
 // PIXI加入指定DOM
 const wrap = document.querySelector("#map_wrap");
 wrap.appendChild(routeMap.canvas);
+
+// render选项监听
+const renderOption = document.querySelector("#renderer");
+renderOption.value = paramUrl.get('render') || 'webgpu';
+renderOption.addEventListener("input", e => {
+  const Name = { "webgpu": "WebGPU", "webgl": "WebGL" }
+  confirm(`请确认是否切换为【${Name[renderOption.value]}】渲染？\n此项操作将不会保存数据，如需保存数据请提前使用【导出】按钮！`) ?
+    (() => {
+      paramUrl.set('render', `${renderOption.value}`);
+      baseUrl.search = paramUrl.toString();
+      window.location.href = baseUrl;
+    })() : renderOption.value = paramUrl.get('render')
+})
 
 let _boolean = true;
 _boolean ? (() => {
